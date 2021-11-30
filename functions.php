254e011d7508887fdea509313c82ee7b0dfb41a3
eehah5ru <nicola.spesivcev@gmail.com>
2021-11-30 18:05:07 +0300

    [setup] stage 6

--- functions.php
+++ functions.php
@@ -7,9 +7,9 @@
  * @package _s
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if ( ! defined( 'TYPO_NEXT_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define( 'TYPO_NEXT_VERSION', '1.0.0' );
 }
 
 if ( ! function_exists( '_s_setup' ) ) :
@@ -140,10 +140,10 @@ add_action( 'widgets_init', '_s_widgets_init' );
  * Enqueue scripts and styles.
  */
 function _s_scripts() {
-	wp_enqueue_style( '_s-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_enqueue_style( '_s-style', get_stylesheet_uri(), array(), TYPO_NEXT_VERSION );
 	wp_style_add_data( '_s-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), TYPO_NEXT_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
