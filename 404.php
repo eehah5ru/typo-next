2e0c78af7fb99cd8525e100c5e03067c733e3e59
eehah5ru <nicola.spesivcev@gmail.com>
2021-11-30 18:06:23 +0300

    [setup] stage 22

--- 404.php
+++ 404.php
@@ -45,8 +45,8 @@ get_header();
 
 					<?php
 					/* translators: %1$s: smiley */
-					$_s_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'typo-next' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$_s_archive_content" );
+					$typo_next_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'typo-next' ), convert_smilies( ':)' ) ) . '</p>';
+					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$typo_next_archive_content" );
 
 					the_widget( 'WP_Widget_Tag_Cloud' );
 					?>
--- inc/custom-header.php
+++ inc/custom-header.php
@@ -14,33 +14,33 @@
 /**
  * Set up the WordPress core custom header feature.
  *
- * @uses _s_header_style()
+ * @uses typo_next_header_style()
  */
-function _s_custom_header_setup() {
+function typo_next_custom_header_setup() {
 	add_theme_support(
 		'custom-header',
 		apply_filters(
-			'_s_custom_header_args',
+			'typo_next_custom_header_args',
 			array(
 				'default-image'      => '',
 				'default-text-color' => '000000',
 				'width'              => 1000,
 				'height'             => 250,
 				'flex-height'        => true,
-				'wp-head-callback'   => '_s_header_style',
+				'wp-head-callback'   => 'typo_next_header_style',
 			)
 		)
 	);
 }
-add_action( 'after_setup_theme', '_s_custom_header_setup' );
+add_action( 'after_setup_theme', 'typo_next_custom_header_setup' );
 
-if ( ! function_exists( '_s_header_style' ) ) :
+if ( ! function_exists( 'typo_next_header_style' ) ) :
 	/**
 	 * Styles the header image and text displayed on the blog.
 	 *
-	 * @see _s_custom_header_setup().
+	 * @see typo_next_custom_header_setup().
 	 */
-	function _s_header_style() {
+	function typo_next_header_style() {
 		$header_text_color = get_header_textcolor();
 
 		/*
