c6be70eff5bc6fc64999e46862407253e4e2a097
eehah5ru <nicola.spesivcev@gmail.com>
2021-12-17 22:12:22 +0300

    [category][index] add basic style for category inddex page

--- archive.php
+++ archive.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying archive pages
  *
@@ -10,41 +11,62 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main category-page">
+    <section class="category-about">
+        <p>
+            «Типография» — это центр современного искусства.
+        </p>
+        <p>
+            Мы делаем <a>выставки</a>, <a>кино</a>, <a>лекции</a>, создаём <a>архив</a> современного искусства Краснодара. В «Типографии» работает <a>библиотека</a>, <a>коворкинг</a>, <a>галерея RedGift</a>, <a>детская лаборатория творчества</a>, винтажный магазин <a>Santa Rrrita</a> и, конечно, <a>Краснодарский институт современного искусства</a> — регулярный образовательный курс по современному искусству. Ещё у нас можно <a>провести мероприятие</a>. Подробнее <a>про нас</a>.
+        </p>
+        <p>
+            Вы можете <a>поддержать</a> нас.
+        </p>
+        <p>
+            Мы работаем с 12:00 до 21:00 по адресу Краснодар, Зиповская 5 к33.
+        </p>
+        <p>
+            Следите за нами в наших социальных сетях: <a>Facebook</a>, <a>Instagram</a>, <a>Vkontakte</a>.
+        </p>
+    </section>
 
-		<?php if ( have_posts() ) : ?>
+    <section class="category-events">
+        <header class="event-header">
+            <?php
+            the_archive_title('<a class="page-title">', '<a>');
+            the_archive_description('<div class="archive-description">', '</div>');
+            ?>
+        </header>
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+        <div class="events-container">
+            <?php
+            $args = array(
+                'posts_per_page'   => 20,
+                'post_type'        => 'post',
+            );
+            $wp_query = new WP_Query($args);
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+            while (have_posts()) :
+                the_post();
+            ?>
+                <article class=" post">
+                    <header>
+                        <?php the_category(', '); ?>
+                    </header>
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+                    <?php typo_next_post_thumbnail(); ?>
+                    <h2>
+                        <?php the_title(); ?>
+                    </h2>
+                </article>
 
-			endwhile;
+            <?php
+            endwhile; // End of the loop.
+            ?>
+        </div>
+    </section>    
 
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main><!-- #main -->
+</main><!-- #main -->
 
 <?php
 get_sidebar();
